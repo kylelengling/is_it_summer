04014edf4047ec795478d96113a9f1ffa4c9bc97
Kyle Lengling <kylelengling@Kyles-MacBook-Pro.local>
2016-04-07 21:51:12 -0500

    Start building JS logic.

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+
+// Gets weather info from forecast.io API
+$( document ).ready(function() {
+  
+});
+
+// Renders forecast.io JSON into HTML
+
+$(document).ready(function() {
+
+});
+
+// Logic for if it's summer or not
+$(document).ready(function() {
+   
+    var currentDate =   //checks for current date 
+    
+    
+    // If date is between June 21st and Sept 22nd and temp above 65
+    if () {
+       
+    }
+    // If date is between June 21st and Sept 22nd and temp not above 65
+    else if () {
+        
+    }
+    // If date is not between June 21st and Sept 22nd
+    else  () {
+        
+    }
+   
+});
