c2c0f68dc32f53ebe9e6d6c4917aa35d05b90780
Kyle Lengling <kylelengling@gmail.com>
2017-01-07 14:32:04 -0600

    update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,26 +7,52 @@ $( document ).ready(function() {
 // Renders forecast.io JSON into HTML
 
 $(document).ready(function() {
+    //render weather icon based on current conditions
+    function() {
+       //rainy
+       
+       //cloudy
+       
+       //sunny
+       
+       //overcast
+       
+       //partly cloudly
+       
+      
+    }
+
 
 });
 
 // Logic for if it's summer or not
 $(document).ready(function() {
    
-    var currentDate =   //checks for current date 
-    
-    
-    // If date is between June 21st and Sept 22nd and temp above 65
-    if () {
-       
+    //variables 
+
+    var d = newDate();  //gets date object 
+    var month = d.getMonth();  // 0 to 11,  0=Jan, 11=December. 
+    var day = d.getDay(); //
+
+    var temperature = temp info from json
+
+    var date = month.concat(day);  //combines month and date into single numeric string
+
+    //check if date between 522 and 820
+
+    if (date >=522 && date < 820 && temperature > 64)  {
+    //yup seems like summer
+    };
+
+    else if (date >=522 && date < 820 && temperature < 64)  {
+    //lol it may be summer but it sure don't feel like it right now
     }
-    // If date is between June 21st and Sept 22nd and temp not above 65
-    else if () {
-        
-    }
-    // If date is not between June 21st and Sept 22nd
-    else  () {
-        
+
+    else if (date >= 820 && date <= 1131) {
+    //awww hell nah it's fucking cold out man.
+
+
+    else (date < 522) {
     }
    
 });
